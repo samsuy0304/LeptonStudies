440a0bdbccf4b47d2acb54ce0bd5ce5bdf18364c
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-06-16 17:13:33 -0500

    Use getLabel(), add comments.

--- python/makePlots.py
+++ python/makePlots.py
@@ -11,13 +11,22 @@ ROOT.gROOT.SetBatch(ROOT.kTRUE)
 # Tell ROOT not to be in charge of memory, fix issue of histograms being deleted when ROOT file is closed:
 ROOT.TH1.AddDirectory(False)
 
-def plot(hist, sample_name, plot_dir, plot_name):
+# get label based on a key
+def getLabel(key):
+    labels = {
+        "pt"    : "p_{T} [GeV]",
+        "eta"   : "#eta",
+    }
+    return labels[key]
+
+# plot a histogram
+def plotHist(hist, sample_name, plot_dir, plot_name, variable):
     c = ROOT.TCanvas("c", "c", 800, 800)
     c.SetLeftMargin(0.15)
 
     # setup
     title = plot_name
-    x_title = "p_{T} [GeV]"
+    x_title = getLabel(variable) 
     y_title = "Entries"
     y_min = 0
     y_max = 3000
@@ -33,11 +42,15 @@ def plot(hist, sample_name, plot_dir, plot_name):
     c.Update()
     c.SaveAs(output_name + ".pdf")
 
+# loop over tree, fill and plot histograms
 def run(plot_dir, sample_name, tree):
     verbose     = False
     n_events    = tree.GetEntries()
+    
     # histograms
     h_LowPtElectron_pt = ROOT.TH1F("h_LowPtElectron_pt", "h_LowPtElectron_pt", 20, 0.0, 20.0)
+    
+    # loop over events
     for i in range(n_events):
         if i % 1000 == 0:
             print("Event {0}".format(i))
@@ -52,8 +65,11 @@ def run(plot_dir, sample_name, tree):
             if verbose:
                 print("LowPtElectron {0}: pt = {1:.3f}, eta = {2:.3f}, phi = {3:.3f}, mass = {4:.3f}".format(j, LowPtElectron_pt[j], LowPtElectron_eta[j], LowPtElectron_phi[j], LowPtElectron_mass[j]))
             h_LowPtElectron_pt.Fill(LowPtElectron_pt[j])
-    plot(h_LowPtElectron_pt, sample_name, plot_dir, "h_LowPtElectron_pt")
+    
+    # plot histograms
+    plotHist(h_LowPtElectron_pt, sample_name, plot_dir, "LowPtElectron_pt", "pt")
 
+# run over input file
 def makePlots():
     input_file  = "/uscms/home/caleb/nobackup/KU_Compressed_SUSY/samples/SMS-T2-4bd_genMET-80_mStop-500_mLSP-490_TuneCP5_13TeV-madgraphMLM-pythia8_NanoAODv9/4153AE9C-1215-A847-8E0A-DEBE98140664.root"
     plot_dir    = "plots"
