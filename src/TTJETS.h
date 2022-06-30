315c83e2e7d9259cef530dce788b3d83d269cab0
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2022-06-30 03:10:40 -0500

    Update TTJETS.h

--- src/TTJETS.h
+++ src/TTJETS.h
@@ -3366,6 +3366,9 @@ public :
    virtual void         Loop();
    virtual void         PlotHist(TH1F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable);
    virtual void         SetupHist(TH1F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
+   virtual void         PlotHist2(TH2F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable);
+   virtual void         SetupHist2(TH2F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
+   
    virtual std::string  GetLabel(std::string variable);
    virtual Bool_t       Notify();
    virtual void         Show(Long64_t entry = -1);
