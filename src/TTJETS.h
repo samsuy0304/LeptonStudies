22d094d4be82170d0b45135b1d77fde0968b66f2
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2022-07-05 01:36:26 -0500

    Added new variable to plots2

--- src/TTJETS.h
+++ src/TTJETS.h
@@ -3366,7 +3366,7 @@ public :
    virtual void         Loop();
    virtual void         PlotHist(TH1F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable);
    virtual void         SetupHist(TH1F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
-   virtual void         PlotHist2(TH2F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable);
+   virtual void         PlotHist2(TH2F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable, std::string variable2);
    virtual void         SetupHist2(TH2F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
    
    virtual std::string  GetLabel(std::string variable);
