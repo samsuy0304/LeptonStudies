fd8170ce7db60f4777dfabd515eeb490c5982f74
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2022-07-08 10:40:35 -0500

    Update TTJETS.h

--- src/TTJETS.h
+++ src/TTJETS.h
@@ -3362,7 +3362,8 @@ public :
    virtual Int_t        Cut(Long64_t entry);
    virtual Int_t        GetEntry(Long64_t entry);
    virtual Long64_t     LoadTree(Long64_t entry);
-    virtual void         Loop();
+   virtual void         Init(TTree *tree);
+   virtual void         Loop();
    virtual void         Loop2();
    virtual void         PlotHist(TH1F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable);
    virtual void         SetupHist(TH1F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
