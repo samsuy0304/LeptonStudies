b9e7f800386dceefb3819fc498d2bca0f4d39ba3
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2022-07-09 02:48:03 -0500

    Update TTJETS.h

--- src/TTJETS.h
+++ src/TTJETS.h
@@ -3365,7 +3365,7 @@ public :
    virtual void         Init(TTree *tree);
    virtual void         Loop();
    virtual void         Loop2();
-   virtual void         PlotHist(TH1F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable);
+   virtual void         PlotHist(TH1F &hist, std::string sample_name, std::string plot_dir,std::string specific, std::string plot_name, std::string variable);
    virtual void         SetupHist(TH1F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
    virtual void         PlotHist2(TH2F &hist, std::string sample_name, std::string plot_dir, std::string plot_name, std::string variable, std::string variable2);
    virtual void         SetupHist2(TH2F &hist, std::string title, std::string x_title, std::string y_title, int color, int line_width);
